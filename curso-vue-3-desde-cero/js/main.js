a529227982c8300cc9067a93157e9535367b2001
Santiago Gonzalez <santiagogonzaric1015@gmail.com>
2023-04-12 21:47:06 -0500

    mostrar favoritos con cache y transiciones

--- curso-vue-3-desde-cero/js/main.js
+++ curso-vue-3-desde-cero/js/main.js
@@ -1,4 +1,5 @@
 const API = "https://api.github.com/users/";
+const requestMaxTime = 3000
 const app = Vue.createApp({
     data() {
         return {
@@ -8,9 +9,16 @@ const app = Vue.createApp({
             favorites: new Map()
         };
     },
+    created(){
+        const savedFavorites = JSON.parse( window.localStorage.getItem('favorites'))
+        if (savedFavorites?.length) {
+            const favorites = new Map(savedFavorites.map(favorite => [favorite.login, favorite]))
+            this.favorites = favorites
+        }
+    },
     computed:{
         isFavorite(){
-            return this.favorites.has(this.result.id)
+            return this.favorites.has(this.result.login)
         },
         allFavorites(){
             return Array.from(this.favorites.values())
@@ -20,12 +28,30 @@ const app = Vue.createApp({
         async doSearch() {
 
             this.result = this.error = null
+            const foubdInFavorites = this.favorites.get(this.search)
+
+            const shouldrequestAgain = (() => {
+                if (!!foubdInFavorites) {
+                    const { lastRequestTime } = foubdInFavorites
+                    const now =Date.now()
+                    return (now - lastRequestTime) > requestMaxTime
+                }
+                return false
+            })()//esto es un iife
+
+            if (!!foubdInFavorites && !shouldrequestAgain) {
+                console.log("buscado con la cahe");
+                return this.result = foubdInFavorites
+            }
+
             try {
+                console.log("no encontrado o cache es antigua");
                 let response = await fetch(API + this.search)
                 if (!response.ok) throw new Error("Usuario no encontrado")
                 const data = await response.json()
                 console.log(data);
                 this.result=data
+                foubdInFavorites.lastRequestTime = Date.now()
             } catch (error) {
                 this.error= error
             } finally{
@@ -33,12 +59,16 @@ const app = Vue.createApp({
             }
         },
         addFavorite() {
-            this.favorites.set(this.result.id, this.result)
+            this.result.lastRequestTime = Date.now()
+            this.favorites.set(this.result.login, this.result)
             this.updateStorage()
         },
         removeFavorite() {
-            this.favorites.delete(this.result.id)
+            this.favorites.delete(this.result.login)
             this.updateStorage()
+        },
+        showFavorite(favorite){
+            this.result = favorite
         },
         updateStorage(){
             window.localStorage.setItem('favorites',JSON.stringify(this.allFavorites))
